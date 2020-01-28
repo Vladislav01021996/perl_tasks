b170c30c65c1b4cc14a875f508feb5fcc1b2098c
Vladislav01021996 <60355305+Vladislav01021996@users.noreply.github.com>
2020-01-28 13:52:51 +0300

    Add files via upload

--- task1.pl
+++ task1.pl
@@ -1,2 +1,10 @@
 use strict;
 use warnings;
+
+
+my @nums = qw(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20);
+print "Массив через пробел - my @nums\n";
+
+
+splice(@nums, 10, 4, 21..24);
+print "Замена элемента - @nums\n";
