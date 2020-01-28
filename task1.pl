aa33652a5b635f8756a3883bb86b6c01bc73b927
MaxMurzins <56456881+MaxMurzins@users.noreply.github.com>
2020-01-28 17:14:32 +0300

    Fix for task1

--- task1.pl
+++ task1.pl
@@ -1,11 +1,12 @@
 use strict;
 use warnings;
-use List::Util 'shuffle';
+use List::Util 'shuffle'; я такого даже не знаю, пользовательские библиотеки это страшно
 
-
+Ты вот решил задачу, но зачем то дописал целую ненужную функцию
 my @array = qw(10 23 32 12 4 16 33);
 print join(' ', sort {$a<=>$b} @array );
 
+Зачем нужна эта функция? Она делает странные вещи 
 sub fisher_yates_shuffle {
 
     my $deck = shift;  
