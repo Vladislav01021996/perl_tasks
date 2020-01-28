c6ae9e4ca129b783b9d912bbf178dabe2400c3fc
Vladislav01021996 <60355305+Vladislav01021996@users.noreply.github.com>
2020-01-28 14:09:38 +0300

    Add files via upload

--- task1.pl
+++ task1.pl
@@ -2,9 +2,5 @@ use strict;
 use warnings;
 
 
-my @nums = qw(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20);
-print "Массив через пробел - my @nums\n";
-
-
-splice(@nums, 10, 4, 21..24);
-print "Замена элемента - @nums\n";
+my @array = qw(1 2 3 100 500 20 8);
+print join(' ', sort {$a<=>$b} @array );
