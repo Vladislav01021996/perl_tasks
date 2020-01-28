3b8a0e348a6e515ab56e53c2ba39c3a6f69fd8a0
Vladislav01021996 <60355305+Vladislav01021996@users.noreply.github.com>
2020-01-28 15:22:49 +0300

    Add files via upload

--- task1.pl
+++ task1.pl
@@ -1,6 +1,20 @@
 use strict;
 use warnings;
+use List::Util 'shuffle';
 
 
-my @array = qw(1 2 3 100 500 20 8);
+my @array = qw(10 23 32 12 4 16 33);
 print join(' ', sort {$a<=>$b} @array );
+
+sub fisher_yates_shuffle {
+
+    my $deck = shift;  
+    return unless @$deck; 
+
+    my $i = @$deck;
+    while (--$i) {
+        my $j = int rand ($i+1);
+        @$deck[$i,$j] = @$deck[$j,$i];
+    }
+}
+
